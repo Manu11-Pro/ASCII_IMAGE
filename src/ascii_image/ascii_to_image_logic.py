4fe19f589f3ec937e9e461ad44e11ad6532a1dcf
Manu(Manu11-Pro) <194583214+Manu11-Pro@users.noreply.github.com>
2026-04-25 14:10:00 +0530

    Refactor Programmed ascii_to_image_logic.py

--- src/ascii_image/ascii_to_image_logic.py
+++ src/ascii_image/ascii_to_image_logic.py
@@ -1,4 +1,4 @@
-from utils import file_to_upload
+from utils import path_of_file_to_upload ,filename
 
 def char_to_use(val):
     if val > 230:
@@ -23,3 +23,37 @@ def char_to_use(val):
         return "@"
     else:
         return "$"
+
+with open(path_of_file_to_upload, "rb") as i:
+    i.seek(18)
+    width = int.from_bytes(i.read(4), "little")
+    height = int.from_bytes(i.read(4), "little")
+    padding = (4 -(width * 3 % 4))% 4
+
+    print(width, height, padding)
+
+    i.seek(54)
+
+    every_rows = []
+
+    for h in range(height):
+        string_row = ""
+
+        for w in range(width):
+            pixel_data = i.read(3)
+
+            B = pixel_data[0]
+            G = pixel_data[1]
+            R = pixel_data[2]
+
+            val = ((0.299 * R) + (0.587 * G) + (0.114 * B))
+            string_row += char_to_use(val)
+
+        i.read(padding)
+        every_rows.append(string_row)
+
+output = f"{filename}.txt"
+
+with open(output, "w") as j:
+    for row in reversed (every_rows):
+        j.write(row + "\n")
